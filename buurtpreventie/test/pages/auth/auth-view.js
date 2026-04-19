3d77b95defcfda519d11d8f1559460950a67d71a
ikhouvankaasq <melle1337k+ikhouvankaasq@gmail.com>
2026-04-19 11:01:52 +0200

    Delete buurtpreventie/test directory

--- buurtpreventie/test/pages/auth/auth-view.js
+++
@@ -1,9 +0,0 @@
-import { showLoginForm, showOnboardingForm } from './auth.js';
-
-export function handleAuthView(options = {}) {
-    if (options.onboarding) {
-        showOnboardingForm();
-    } else {
-        showLoginForm();
-    }
-}
